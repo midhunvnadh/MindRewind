0a4c7c98c1988d81246d3fa8c656bf6b73721fd0
midhunvnadh <midhun.vnadh5732@gmail.com>
2019-10-22 00:02:10 +0530

    Made the user folder and designed it, adjusted the form on home page.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,43 @@
+var returnDay = (arg) => {
+  $date = new Date(arg);
+  return getTheDay($date);
+}
+function getTheDay(date){
+  $a = date.getDay();
+  if($a == 1){
+    $theDay = "Monday";
+  }else if($a == 2){
+    $theDay = "Tuesday";
+  }else if($a == 3){
+    $theDay = "Wednesday";
+  }else if($a == 4){
+    $theDay = "Thursday";
+  }else if($a == 5){
+    $theDay = "Friday";
+  }else if($a == 6){
+    $theDay = "Saturday";
+  }else if($a == 0){
+    $theDay = "Sunday";
+  }
+  return $theDay;
+}
 $(document).ready(function(){
     $("video").on("loadstart", function(){
       $(".abs-over").css("background","rgba(225,225,225,0.45)");
     });
+    Number.prototype.AddZero= function(b,c){
+        var  l= (String(b|| 10).length - String(this).length)+1;
+        return l> 0? new Array(l).join(c|| '0')+this : this;
+     }
+     var d = new Date(),
+       localDateTime= [(d.getFullYear()).AddZero(),d.getMonth().AddZero(),
+                d.getDate()+1].join('-') +'T' +
+               [d.getHours().AddZero(),
+                d.getMinutes().AddZero()].join(':');
+       var elem=document.getElementById("datePicker");
+       elem.value = localDateTime;
+      $('.day').text(getTheDay(new Date()));
+      $("#datePicker").on('input',function(){
+        $('.day').text(returnDay($('#datePicker').val()));
+      });
 });
