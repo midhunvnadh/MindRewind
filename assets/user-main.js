c8e9804cc8829c0907a1d973aceff91d7f32f4b4
midhunvnadh <32438353+midhunvnadh@users.noreply.github.com>
2019-11-01 21:05:34 +0530

    Changed User UI styling
    Changed the User Panel UI.

--- assets/user-main.js
+++ assets/user-main.js
@@ -18,28 +18,21 @@ $(document).ready(function(){
       $(this).addClass("active-link");
       console.log($(this).attr("data-trigger"));
     }
-  });
-  $(window).resize(function(){
-    $(".main-userlg").width(($(window).width() - $('.side-nav-user').width()) + "px");
-  });
-  $(window).resize();
-  new ResizeSensor($('.side-nav-user'), function(){
-      $(window).resize();
+    else if($(this).attr("data-trigger") == null){
+      if($toggled == true)
+        $(".side-nav-user .logo .toggle-nav").click();
+    }
   });
   $toggled = false;
-  $(".side-nav-user .logo .toggle-nav").click(function(){
+  $(".tools-bars-dp .logo .toggle-nav").click(function(){
     if(!$toggled){
-      $(this).html('<i class="fas fa-arrow-right">');
       $(".side-nav-user").css("--nav-side-width","70px");
       $(".side-nav-user ul .nav-links-expanded").hide();
-      $(".tools-bars-dp").css("left","70px");
       $toggled = true;
     }
     else{
-      $(this).html('<i class="fas fa-arrow-left"></i> Collapse');
-      $(".side-nav-user").css("--nav-side-width","200px");
+      $(".side-nav-user").css("--nav-side-width","100%");
       $(".side-nav-user ul .nav-links-expanded").show();
-      $(".tools-bars-dp").css("left","200px");
       $toggled = false;
     }
   });
